9dbf1f98200d30b5e6eac964231e4a8f08f85a24
pbotecchi-daitan <pbotecchi@daitan.com>
2024-01-08 11:54:59 -0300

    [E4E-1124]: Bumping dependencies

--- jest.config.js
+++ jest.config.js
@@ -12,16 +12,16 @@ module.exports = {
       statements: 100,
     },
   },
-  globals: {
-    'ts-jest': {
-      diagnostics: false,
-      tsconfig: './tsconfig.json',
-    },
-  },
   maxWorkers: os.cpus().length / 2,
   preset: 'ts-jest',
   roots: ['<rootDir>/lib'],
   transform: {
-    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        diagnostics: false,
+        tsconfig: './tsconfig.json',
+      },
+    ],
   },
 };
